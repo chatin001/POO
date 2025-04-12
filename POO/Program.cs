6262dce0c6e423247c1ab9cfee44a0a46b71042b
chatin001 <148296751+chatin001@users.noreply.github.com>
2025-04-11 22:38:38 -0500

    Listo

--- POO/Program.cs
+++ POO/Program.cs
@@ -8,8 +8,9 @@
 
             Console.WriteLine(song1.PublicAlbum);
             Console.WriteLine(song1.PublicTitle);
+            Console.WriteLine(song1.PublicDescription);
+            Console.WriteLine(song1.PublicGenre);
 
-
-    }
+        }
     }
 }
